7562aa2bcf796427b46349716d9c6669706b16b5
Theod0reWu <twu00@stuy.edu>
2019-11-13 21:11:53 -0500

    added makefile

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
+#include "stat.h"
 
+int main(int argc, char const *argv[]) {
+  struct stat info;
+  stat("README.md", &info);
+  printf("%i\n", info.st_size);
+  printf("%i\n", info.st_mode);
+  printf("%s\n", ctime(info.st_atime));
+  return 0;
+}
